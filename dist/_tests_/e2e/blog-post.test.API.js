cc61976a17e735fc7cef23b1b2e8bea68a5b8ca8
Sh-d-l <96614868+Sh-d-l@users.noreply.github.com>
2023-03-26 14:49:20 +0300

    Delete dist directory

--- dist/_tests_/e2e/blog-post.test.API.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-describe('/blogs', () => {
-    it("should response 200");
-});
-//# sourceMappingURL=blog-post.test.API.js.map
--- dist/src/deleteAll/deleteAll.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=deleteAll.js.map
