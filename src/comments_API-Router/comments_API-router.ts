b3c8ecdddd5564653e2364fbbf37fb5fdd4d97bd
sh-d-l <sh_d_l@mail.ru>
2023-06-09 13:11:36 +0300

    13-11 09.06.23 add 6 home work

--- src/comments_API-Router/comments_API-router.ts
+++ src/comments_API-Router/comments_API-router.ts
@@ -2,27 +2,26 @@ import {Response, Request, Router} from "express";
 import {authMiddleware} from "../middlewares/validators/authMiddleware";
 import {commentsService} from "../comments_API-service/comments_API-service";
 import {createCommentValidation} from "../middlewares/validators/validations";
-import {jwtService} from "../application/jwt-service";
 import {CommentType} from "../post_API-repositories/post_API-repositories-db";
 
 export const commentsRouter = Router({})
 
-commentsRouter.get("/:id", async (req:Request, res:Response) => {
+commentsRouter.get("/:id", async (req: Request, res: Response) => {
     const getCommentById = await commentsService.getCommentById(req.params.id)
-    if(getCommentById) {
+    if (getCommentById) {
         res.status(200).send(getCommentById)
-    }
-    else {
+    } else {
         res.sendStatus(404)
     }
 })
 commentsRouter.put("/:commentId",
     authMiddleware,
     ...createCommentValidation,
-    async (req:Request, res:Response) => {
-        const getCommentById:CommentType | null = await commentsService.getCommentById(req.params.commentId)
-        if(req.user?.id == getCommentById.commentatorInfo.userId)
-
+    async (req: Request, res: Response) => {
+        const getCommentById: CommentType | null = await commentsService.getCommentById(req.params.commentId)
+        if (req.user?.id !== getCommentById?.commentatorInfo.userId) {
+            res.sendStatus(403)
+        }
         const commentUpdate: boolean = await commentsService.commentUpdate(req.params.commentId, req.body.comment)
         if (commentUpdate) {
             res.sendStatus(204)
@@ -32,12 +31,15 @@ commentsRouter.put("/:commentId",
     })
 commentsRouter.delete("/:commentId",
     authMiddleware,
-    async (req:Request, res:Response) => {
-    const delComment:boolean = await commentsService.commentDelete(req.params.commentId)
-    if(delComment) {
-        res.sendStatus(204)
-    }
-    else {
-        res.sendStatus(404)
-    }
-})
+    async (req: Request, res: Response) => {
+        const getCommentById: CommentType | null = await commentsService.getCommentById(req.params.commentId)
+        if (req.user?.id !== getCommentById?.commentatorInfo.userId) {
+            res.sendStatus(403)
+        }
+        const delComment: boolean = await commentsService.commentDelete(req.params.commentId)
+        if (delComment) {
+            res.sendStatus(204)
+        } else {
+            res.sendStatus(404)
+        }
+    })
