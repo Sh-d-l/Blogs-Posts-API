6584973bd35d0d4ff0fa3ad7e1daaa0dbf75a9b8
sh-d-l <sh_d_l@mail.ru>
2023-10-22 18:09:51 +0300

    18-09 22.10.23 oop

--- src/service/comments_API-service.ts
+++ src/service/comments_API-service.ts
@@ -7,9 +7,17 @@ export class CommentsService {
         this.commentsRepo = new CommentsRepo()
     }
     async makeLikeService(id:string, likeStatus: string):Promise<boolean> {
+        const commentById = await this.commentsRepo.getCommentById(id)
+        if
+        let likesCount = 0;
+        let dislikesCount = 0;
         if (likeStatus === "like") {
-            const likeCount =
+            likesCount += 1;
         }
+        if (likeStatus === "Dislike") {
+            dislikesCount += 1;
+        }
+
         return await this.commentsRepo.updateLikesInfo(id,likeStatus)
     }
 
